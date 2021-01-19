14dc6df6caf144030b1e81fc0897622c8af020a5
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-19 14:15:17 +0000

    Getting back to where we were yesterday

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source "https://rubygems.org"
+
+ruby '2.6.5'
+
 gem 'rspec', '~> 3.6'
+gem 'shotgun'
 gem 'sinatra'
 gem 'webrick'
--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
+require 'shotgun'
 
+set :session_secret, 'super secret'
 
 get '/' do
-  "Hello World"
+  "Hello you foolish duck"
+end
+
+get '/secret' do
+  "You know all my inner thoughts"
+end
+
+get '/HollySecret' do
+  "I am a magician"
 end
