7cce411c4ad09d408331ca522ac2f6351bd9d0a8
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-20 16:44:48 +0000

    Created play.erb. Updated Gemfile.lock, app.rb, testing_infrastructure_spec.rb, and index.erb. This reflects Entering Players.

--- app.rb
+++ app.rb
@@ -7,12 +7,23 @@ require 'shotgun'
 #set :session_secret, 'super secret'
 
 class Battle < Sinatra::Base
-  get '/' do 
+  get '/test_infrastructure' do
     'Testing infrastructure working!'
   end
 
+  get '/' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
+  end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
-end 
-
-
+end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,43 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do 
-    visit('/')
+  scenario 'Can run app and check page content' do
+    visit('/test_infrastructure')
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
+feature 'Entering players' do
+  scenario 'Displays page content instructions' do
+    visit('/')
+    expect(page).to have_content('Please enter your names!')
+  end
+
+  scenario 'Displays Submit button' do
+    visit ('/')
+    expect(page).to have_button('Submit')
+  end
+
+  scenario 'Accepts submitted names' do
+    visit('/')
+    fill_in 'player_1_name', with: 'Alfred'
+    fill_in 'player_2_name', with: 'Bruce'
+    click_button 'Submit'
+    expect(page).to have_content('Player 1 Name = Alfred')
+  end
+end
+
+
+
+=begin
+post '/named-rabbit' do
+  p params
+  @rabbit_name = params[:name]
+  params[:age]
+  params[:breed]
+  erb(:index)
+end
+
+<form action ='/named-rabbit' method = "post" >
+  <input type='text' name='name'>
+  <input type='submit' value='Submit'>
+</form>
+=end
